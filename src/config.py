5c78ede2e969c7253ff69f2e6b472c01c19921bd
kadisonm <134670047+kadisonm@users.noreply.github.com>
2024-12-18 15:27:39 +1030

    Push config

--- src/config.py
+++ src/config.py
@@ -1,22 +1,21 @@
-from typing import Dict
 from aqt import mw
-from aqt.qt import *
 
 config = {
-    'deck': "0",
+    'deck': "None",
 }
 
 def save_config() -> None:
     mw.addonManager.writeConfig(__name__, config)
 
-def load_config() -> Dict:
+def load_config():
     global config
 
     foundConfig = mw.addonManager.getConfig(__name__)
+
+    print(foundConfig)
 
     if foundConfig:
         config = foundConfig
     else:
         save_config()
 
-    return config
---
+++ src/kanji.py
@@ -0,0 +1,11 @@
+def get_keywords():
+    print("yeh")
+
+def get_primitives():
+    print("yeh")
+
+def get_kanji():
+    print("yeh")
+
+def create_kanji_cards():
+    print("yeh")
