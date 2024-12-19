f936879b643ba87e5a2c26ecebec4deec0fd1a47
Kaius <134670047+kadisonm@users.noreply.github.com>
2024-12-19 21:00:16 +1030

    Add card creation

--- src/init.py
+++ src/init.py
@@ -3,8 +3,8 @@ from aqt.utils import showInfo, qconnect
 from aqt.qt import *
 from .config import load_config, get_config, save_config
 from .configWindow import ConfigWindow
-from .kanji import get_heisig_keyword, get_radicals
 from anki.notes import Note
+from .kanji import scan_note, get_heisig_keyword, get_radicals
 
 def start():
     load_config()
@@ -19,13 +19,18 @@ def start():
     # Add config action
     mw.addonManager.setConfigAction(__name__, window.open)
 
-    def addedNote(note: Note):
+    def addedNote(note: Note):  
+        if note.has_tag("kanji-learner"):
+            return
+
+        deckId = get_config()["deck_id"]
+
         result = mw.col.db.scalar(
-            "SELECT 1 FROM cards WHERE nid = ? AND did = ?", note.id, get_config()["deck_id"]
+            "SELECT 1 FROM cards WHERE nid = ? AND did = ?", note.id, deckId
         )
 
-        # if result:
-            # Create card
+        if result:
+            scan_note(note, deckId)
             
     gui_hooks.add_cards_did_add_note.append(addedNote)
 
